3bf1284688d2cc984b5de0e69d4182f0cf3f9f05
Горечко Владислав <brianprott456@gmail.com>
2026-06-07 14:22:16 +0300

    Fix remaining flake8 errors

--- user.py
+++ user.py
@@ -1,10 +1,12 @@
 from interfaces import Observer
 
+
 class User:
     def __init__(self, user_id: int, name: str, email: str):
         self.user_id = user_id
         self.name = name
         self.email = email
+
 
 class Reader(User, Observer):
     def __init__(self, user_id: int, name: str, email: str):
